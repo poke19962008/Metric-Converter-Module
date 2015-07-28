1a248b339dac5311bda89b11cad3ea2993ad4503
Sayan Das <poke19962008@gmail.com>
2015-07-28 15:04:47 +0530

    added comments and pass the value from #search to checkMetric.py

--- js/search.js
+++ js/search.js
@@ -4,18 +4,33 @@ $( function() {
     
 });
 
+
+/*
+    Runs checkMetric.py to get:
+    {"isMetricQuery": "true", "lhsUnit": "<some Unit>", "rhsUnit": "<some Unit>", "metric": "<some metric>", "lhsValue": "<some Value>"}
+    if the query needs metric converter
+    
+    else:
+    {"isMetricQuery": "false", "lhsUnit": "-", "rhsUnit": "-", "metric": "-", "lhsValue": "-"}
+    
+    Then calls metricDiv.html
+    sets the value of elements retrieved from checkMetric.py
+    
+    Sample Queries:
+    convert 123celsius to fahrenheit <type: 1>
+    convert 123 celsius to fahrenheit <type: 2>
+*/
 function metricQuery(){
     
     $.ajax({                
             async   : true,
             url     : 'http://localhost:8000/cgi-bin/checkMetric.py',
             type    : 'post',
-            data    : {},
+            data    : {q: $("#search").val().trim()},
             dataType: 'text',
             error   : function() {console.log("error...");},
 
         }).done (function(text) {
-
         var res = JSON.parse(text);
 
         if(res['isMetricQuery']){
